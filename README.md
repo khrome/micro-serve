c9310948d993d2b3198d97ca9a816c4b3c1f6035
khrome <abbey@khrome.net>
2018-01-12 13:44:06 -0800

    big changes

--- README.md
+++ README.md
@@ -1,27 +1,45 @@
-whammo on express
-=================
-micro-serve is the express implementation of [whammo](https://www.npmjs.com/package/whammo) which is an ulta-thin server wrapper. Running a library that's intended to be barebones *inside* an application which is something of a kitchen sink may seem nonsensical, but the reality is many hosting configurations use express as an application deployment interface. The intent is to enable express in [Sterling](https://www.npmjs.com/package/sterling), but it can be used in any context as a drop-in replacement for whammo.
+µserve
+======
+micro-serve is simple server wrapper for [whammo](https://www.npmjs.com/package/whammo) usage with no configuration
 
-Usage
------
+Command Line
+------------
+If you installed with `-g` you've got the app in your path. Serve the current directory on 8080 with
 
-This sample uses director, but it is just as easy to use page.js or any other router (or your own)
+	userve -p 8080
 
-Instead of
+for more information consult `userve --help`
 
-	var Server = require('whammo');
-	
-use
 
-	var Server = require('micro-serve').express(expressInstance);
-	
-then continue to work as if you are using [whammo](https://www.npmjs.com/package/whammo).
-
-Testing
+Routing
 -------
-Tests use mocha/should to execute the tests from root
 
-    mocha
+	var Server = require('micro-serve');
+
+Routing with [director](https://www.npmjs.com/package/director) looks like:
+
+	var app = Server({
+		director : {
+			'/test' : {get:function(){
+                this.res.end('Got it!');
+            }}
+		}
+	});
+
+
+Whereas if you want to use the [express router](https://www.npmjs.com/package/router):
+
+	var app = Server({
+		express : {
+			'/test' : {get:function(req, res){
+				res.end('Got it!');
+			}}
+		}
+	});
+
+then regardless of what routes you use, you'll need attach to a port. `app` is a whammo instance.
+
+	app.listen(8080);
 
 If you find any rough edges, please submit a bug!
 
--- server.js
+++ server.js
@@ -1,21 +1,55 @@
-#!/usr/bin/env node
-
 //unassuming server wrapper
 var Server = require('whammo');
 
-var micro = {
-    express : function(expressInstance){
-        var expressMiddleware = require('./frameworks/express.js');
-        return function(){
-            var server = new Server();
-            expressInstance.use(expressMiddleware(server));
-            server.listen = function(port){
-                return expressInstance.listen(port);
-            }
-            return server;
-        }
+var defaultTypes = [
+    'png', 'gif', 'jpg', 'jpeg', 'json', 'js', 'html', 'css',
+    'ttf', 'eot', 'woff', 'ico', 'otf', 'svg'
+];
+
+function microserve(options){
+    if(!options) options = {};
+    var application;
+    if(options.express){
+        application = new Server({
+            actions : [
+                //first look for any static files
+                { name : 'http-file', with:{types:options.types || defaultTypes}},
+                // always load GET vars
+                { name : 'get-vars' },
+                // always load POST vars
+                { name : 'post-vars'},
+                { name : 'director-router', with : {
+                    routes : options.director
+                }},
+                { name : '404' }
+            ]
+        });
     }
-};
+    if(options.director){
+        application = new Server({
+            actions : [
+                //first look for any static files
+                { name : 'http-file', with:{types:options.types || defaultTypes}},
+                // always load GET vars
+                { name : 'get-vars' },
+                // always load POST vars
+                { name : 'post-vars'},
+                { name : 'director-router', with : {
+                    routes : options.director
+                }},
+                { name : '404' }
+            ]
+        });
+    }
+    if(!application){
+        application = new Server({
+            actions : [
+                { name : 'http-file', with:{types:options.types || defaultTypes}},
+                { name : '404' }
+            ]
+        });
+    }
+    return application;
+}
 
-module.exports = micro;
-
+module.exports = microserve;
